201b6743abe502296686385ff53ca1da97cb0820
Francis Bulus <notchera@gmail.com>
2020-02-17 19:23:45 +0100

    completed iterative sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,25 +1,26 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
+        # (hint, can do in 3 loc)
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+                
+        arr[cur_index], arr[smallest_index] = (
+            arr[smallest_index], arr[cur_index])
         # TO-DO: swap
-
-
-
-
     return arr
 
 
+print(selection_sort([3, 4, 9, 2, 10, 25, 6]))
+
+
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
     arr_size = len(arr) - 1
     for i in range(arr_size):
         for j in range(arr_size - i):
@@ -28,8 +29,7 @@ def bubble_sort( arr ):
     return arr
 
 
-
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
 
     return arr
